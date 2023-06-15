b7e302ea04d7637428120fec1e724b363380cf98
Moidhas <moidk3026@gmail.com>
2023-06-15 19:27:00 -0400

    s

--- main.rb
+++ main.rb
@@ -1,35 +1,35 @@
 # frozen_string_literal: true
 
+module Decision
+
+end
+
+
 # Board
 class Board
-  attr_accessor(:p1, :p2, :moves)
+  attr_accessor :moves, :p1, :p2
 
-  def initialize(pl1, pl2)
-    self.p1 = pl1
-    self.p2 = pl2
-    pl1.board = self
-    pl2.board = self
+  def initialize
     self.moves = Array.new(3) { Array.new(3) }
+    self.p1 = Player.new('X', self)
+    self.p2 = Player.new('O', self)
   end
 
   def to_s
-    moves.reduce('') do |acc, list|
-      acc + "#{list}\n"
-    end
+    moves.reduce('') { |acc, list| acc + "#{list}\n" }
   end
 end
 
 # Player
 class Player
-  attr_accessor(:name, :piece, :board)
+  attr_accessor :sym, :board
 
-  def initialize(name, piece)
-    self.name = name
-    self.piece = piece
+  def initialize(symbol, board)
+    self.sym = symbol
+    self.board = board
+  end
+
+  def move(row, column)
+    board.move[row][column] = sym
   end
 end
-
-p1 = Player.new(p1, 'X')
-p2 = Player.new(p2, 'O')
-board = Board.new(p1, p2)
-puts board
