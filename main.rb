057cd07d09e7fcfa0df99c1a744ea4b98734837a
Moidhas <moidk3026@gmail.com>
2023-06-16 13:10:58 -0400

    ROUGH DRAFT implemented winning, and putting pieces down

--- main.rb
+++ main.rb
@@ -19,39 +19,87 @@ class Board
   attr_accessor :moves, :p1, :p2
 
   def initialize
-    self.moves = Array.new(3) { Array.new(3, 0) }
+    self.moves = Array.new(9, 0)
     self.p1 = Player.new('X', self)
     self.p2 = Player.new('O', self)
   end
 
   def to_s
-    moves.reduce('') { |acc, list| acc + "#{list}\n" }
+    index = 1
+    moves.reduce(' ') do |acc, item|
+      if (index % 3).zero?
+        acc + "| #{item} | \n"
+      else
+        acc + "| #{item} |"
+      end
+      index += 1
+      acc
+    end
   end
 
   def play
-    puts self
-    posn = p1.ask
-    moves[posn[0]][posn[1]] = p1.sym
-    puts self
+    display_start
+    loop do
+     posn = p1.ask
+     moves[posn - 1] = p1.sym
+     puts self
+     break if p1.win? || p2.win?
+    end
   end
+
+  private
+  def display_start
+    for i in (1..9)
+      if (i % 3).zero?
+        puts "| #{i} |" 
+      else
+        print "| #{i} |"
+      end
+    end
+  end
+
 end
 
 # Player
 class Player
   include InputTroubles
-  attr_accessor :sym, :board
+  attr_accessor :sym, :board, :positions
+
+  WINNING = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+             [1, 4, 7], [2, 5, 8], [3, 6, 9],
+             [1, 5, 9], [3, 5, 7]].freeze
 
   def initialize(symbol, board)
     self.sym = symbol
     self.board = board
+    self.positions = Array.new(9)
   end
 
   def ask
-    print "#{sym} pick a row: "
-    row = input_move
-    print "\n#{sym} pick a column: "
-    col = input_move
-    [row, col]
+    loop do
+      print "#{sym}, input a number (1 - 9): "
+      posn = input_move
+      if valid_posn(posn)
+        positions.push(posn) 
+        break posn
+      end
+    end
+  end
+
+  def win?
+    WINNING.filter { |winning| winning - positions != [] }.length < 8
+  end
+
+  private 
+  def valid_posn(posn)
+    if !posn.between?(1, 9)
+      puts 'Try again please input a number between 1 and 9.'
+      return false 
+    elsif positions.include?(posn)
+      puts 'The position has already been chosen, Try again.'
+      return false
+    end
+    true
   end
 end
 
